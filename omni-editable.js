a32c35707ff2da84d291e3fc35e8488f37c394c7
ealef <eugene.alefragi@gmail.com>
2015-10-01 20:28:00 +0300

    Buttons Customization
    Customize the class and the text of Edit, Cancel, OK button and the
    class of the text input.
    Also, startEdit and editetText methods added.

--- omni-editable.js
+++ omni-editable.js
@@ -3,13 +3,24 @@
     $.fn.omniEditable = function (options) {
 
         // private variables
-        var EDIT_BTN_CLS = "cancelButton";
+        var EDIT_BTN_CLS = "editButton";
         var OK_BTN_CLS = "okButton";
         var CANCEL_BTN_CLS = "cancelButton";
         var EDITABLE_SPAN_CLS = "editableSpan";
         var TEXT_INPUT_CLS = "textInput";
         var EDIT_MODE_CLS = "editMode";
         var NORMAL_MODE_CLS = "normalMode";
+
+        var EDIT_BTN_STYLE_CLS = "";
+        var EDIT_BTN_TEXT = "Edit";
+        var CANCEL_BTN_STYLE_CLS = "";
+        var CANCEL_BTN_TEXT = "Cancel";
+        var OK_BTN_STYLE_CLS = "";
+        var OK_BTN_TEXT = "OK";
+        var TEXT_INPUT_STYLE_CLS = "";
+
+        var internalOptions = [];
+        
         // ...
 
         // support multiple elements
@@ -19,33 +30,40 @@
         }
 
         
-
-
+       
         // private methods
         var startEdit = function (e) {
             var parent = $(this).parent();
             var editableSpan = parent.find("." + EDITABLE_SPAN_CLS);
 
-            if (options.onStartEdit !== undefined) {
-                options.onStartEdit(e, parent, editableSpan.html());
-            }
+            //if (options != null) {
+                if (internalOptions.onStartEdit !== undefined) {
+                    internalOptions.onStartEdit(e, parent, editableSpan.html());
+                }
+            //}
 
             if (!e.isPropagationStopped()) {
                 var cancelButton = $("<button>Cancel</button>")
-						.text("Cancel")
+						.text(CANCEL_BTN_TEXT)
 						.attr("type", "button")
 						.click(cancelEdit)
-						.addClass(CANCEL_BTN_CLS + " " + EDIT_MODE_CLS)
+						.addClass(CANCEL_BTN_CLS + " " 
+                                + EDIT_MODE_CLS + " " 
+                                + CANCEL_BTN_STYLE_CLS)
 						.insertAfter(editableSpan);
                 var okButton = $("<button></button>")
-						.text("OK")
+						.text(OK_BTN_TEXT)
 						.attr("type", "button")
-						.addClass(OK_BTN_CLS + " " + EDIT_MODE_CLS)
+						.addClass(OK_BTN_CLS + " "
+                        + EDIT_MODE_CLS + " "
+                        + OK_BTN_STYLE_CLS)
 						.click(acceptEdit)
 						.insertAfter(editableSpan);
                 var input = $("<input type = 'text'/>")
 						.attr("class", parent.attr("class"))
-						.addClass(TEXT_INPUT_CLS + " " + EDIT_MODE_CLS)
+						.addClass(TEXT_INPUT_CLS + " "
+                                + EDIT_MODE_CLS + " "
+                                + TEXT_INPUT_STYLE_CLS)
 						.val(editableSpan.text())
 						.prop("defaultValue", editableSpan.text())
 						.keyup(checkShortcuts)
@@ -69,20 +87,23 @@
 
         function exitEditMode(e, clickedButton, keepChanges) {
             var parent = clickedButton.parent();
-            var optionEvent = keepChanges ?
-					options.onAcceptEdit :
-					options.onCancelEdit;
 
-            if (optionEvent !== undefined) {
-                var input = parent.find("." + TEXT_INPUT_CLS);
+            //if (internalOptions != null) {
+                var optionEvent = keepChanges ?
+                        internalOptions.onAcceptEdit :
+                        internalOptions.onCancelEdit;
 
-                optionEvent(
-					e,
-					parent,
-					input.val(),
-					input.prop("defaultValue")
-				);
-            }
+                if (optionEvent !== undefined) {
+                    var input = parent.find("." + TEXT_INPUT_CLS);
+
+                    optionEvent(
+                        e,
+                        parent,
+                        input.val(),
+                        input.prop("defaultValue")
+                    );
+                }
+            //}
 
             if (!e.isPropagationStopped()) {
                 revertEdit(parent, keepChanges);
@@ -131,32 +152,85 @@
         }
 
         function createEditButton(editable) {
+            var classes = EDIT_BTN_CLS + " "
+                        + NORMAL_MODE_CLS + " "
+                        + EDIT_BTN_STYLE_CLS;
+            //console.log(classes);
             return $("<button></button>")
-					.text("Edit")
+					.text(EDIT_BTN_TEXT)
 					.attr("type", "button")
-					.addClass(EDIT_BTN_CLS + " " + NORMAL_MODE_CLS)
+					.addClass(classes)
 					.click(startEdit);
         }
+
+        function initializeOptions(options) {
+            if (options != null) {
+                internalOptions = options;
+            }
+
+            if (internalOptions.buttonClass !== undefined) {
+                EDIT_BTN_STYLE_CLS = internalOptions.buttonClass;
+                CANCEL_BTN_STYLE_CLS = internalOptions.buttonClass;
+                OK_BTN_STYLE_CLS = internalOptions.buttonClass;
+            } else {
+                if (internalOptions.editButtonClass !== undefined) {
+                    EDIT_BTN_STYLE_CLS = internalOptions.editButtonClass;
+                }
+
+                if (internalOptions.cancelButtonClass !== undefined) {
+                    CANCEL_BTN_STYLE_CLS = internalOptions.cancelButtonClass;
+                }
+
+                if (internalOptions.okButtonClass !== undefined) {
+                    OK_BTN_STYLE_CLS = internalOptions.okButtonClass;
+                }
+            }
+
+            if (internalOptions.textInputClass !== undefined) {
+                TEXT_INPUT_STYLE_CLS = internalOptions.textInputClass;
+            }
+            console.log("input class:" + TEXT_INPUT_STYLE_CLS);
+            if (internalOptions.editButtonText !== undefined) {
+                EDIT_BTN_TEXT = internalOptions.editButtonText;
+            }
+
+            if (internalOptions.cancelButtonText !== undefined) {
+                CANCEL_BTN_TEXT = internalOptions.cancelButtonText;
+            }
+
+            if (internalOptions.okButtonText !== undefined) {
+                OK_BTN_TEXT = internalOptions.okButtonText;
+            }
+
+        }
+
         // ...
 
         // public methods        
         this.initialize = function () {
-            // do something ...
+            this.each(function () {
+                var editable = $(this);
+
+                initializeOptions(options);
+                
+                var editableHtml = editable.html();
+                editable.empty()
+                        .append(createSpan(editable, editableHtml))
+                        .append(createEditButton(editable));
+            });
             return this;
         };
 
-        this.bar = function () {
-            // do something ...
+        this.removeEditButton = function () {
+            this.find("." + EDIT_BTN_CLS).remove();
+            console.log("button removed");
         };
+
+        this.editedText = function () {
+            return this.find("."+EDITABLE_SPAN_CLS).html();
+            
+        }
+
         return this.initialize();
-
-        this.each(function () {
-            var editable = $(this);
-
-            var editableHtml = editable.html();
-            editable.empty()
-					.append(createSpan(editable, editableHtml))
-					.append(createEditButton(editable));
-        });
     }
-}(jQuery, true ? { log: function() { } } : console ));
+}(jQuery, false ? { log: function() { } } : console ));
